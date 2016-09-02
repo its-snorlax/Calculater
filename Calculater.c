2a6a98da9478943b3b5715df3115e8e21ff29f5c
Prayas Jain <p.jain3322@gmail.com>
2016-09-02 12:21:01 +0530

    implement sub functionality

--- Calculater.c
+++ Calculater.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <conio.h>
-#include <string.h>
 int add(int number1, int number2){
 	int sum = number1 + number2;
 	return sum;
 }
+int sub(int number1, int number2){
+	int sum = number1 - number2;
+	return sum;
+}
+
 void main() {
 	int number1 , number2	;
 	char symbol;
@@ -12,6 +16,12 @@ void main() {
 	scanf ("%d %c %d",&number1, &symbol, &number2);
 	if (symbol == '+') {
 		int result = add(number1,number2);
-		printf(" sum = %d\n",result);
+		printf ("sum = %d\n",result);
 	}
+	else if (symbol == '-')
+	{
+		int result = sub(number1,number2);
+		printf ("sub = %d\n",result);
+	}
+
 }
