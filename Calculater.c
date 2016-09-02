98289c3e71a0f586f0586997d8352742593242fd
Prayas Jain <p.jain3322@gmail.com>
2016-09-02 12:26:40 +0530

    implement multiply functionality

--- Calculater.c
+++ Calculater.c
@@ -4,9 +4,15 @@ int add(int number1, int number2){
 	int sum = number1 + number2;
 	return sum;
 }
+
 int sub(int number1, int number2){
-	int sum = number1 - number2;
-	return sum;
+	int sub = number1 - number2;
+	return sub;
+}
+
+int mult(int number1, int number2){
+	int mult = number1 * number2;
+	return mult;
 }
 
 void main() {
@@ -16,12 +22,16 @@ void main() {
 	scanf ("%d %c %d",&number1, &symbol, &number2);
 	if (symbol == '+') {
 		int result = add(number1,number2);
-		printf ("sum = %d\n",result);
+		printf ("sum is = %d\n",result);
 	}
 	else if (symbol == '-')
 	{
 		int result = sub(number1,number2);
-		printf ("sub = %d\n",result);
+		printf ("sub is = %d\n",result);
 	}
-
+	else if (symbol == '*')
+	{
+		int result = mult(number1,number2);
+		printf ("product is = %d\n",result);
+	}
 }
