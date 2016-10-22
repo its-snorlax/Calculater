78000b1742ed1c543f22734f028802e5ac58cb33
minijainsahab <p.jain3322@gmail.com>
2016-10-23 00:10:48 +0530

    implement header file of all airthmatic function and ad run.sh f

--- Calculater.c
+++ Calculater.c
@@ -1,47 +1,23 @@
 #include <stdio.h>
-#include <conio.h>
 #include <stdlib.h>
 #include <string.h>
-int add(int number1, int number2){
-	int sum = number1 + number2;
-	return sum;
-}
-
-int sub(int number1, int number2){
-	int sub = number1 - number2;
-	return sub;
-}
-
-int mult(int number1, int number2){
-	int mult = number1 * number2;
-	return mult;
-}
-
-int division(int number1, int number2){
-	int div = number1 / number2;
-	return div;
-}
-
-int main (int argc, char const *argv[]) {
+#include "opration.h"
+int main (int argc, char const *argv[]){
 	int number1 = atoi(argv[1]);
 	int number2 = atoi(argv[3]);
 
-	if (strcmp(argv[2] , "+") == 0) {
-		int result = add (number1 , number2);
-		printf ("sum is = %d\n", result);
+	if (strcmp(argv[2] , "+") == 0){
+		printf("Sum = %d \n",add(number1,number2));
 	}
-	else if (strcmp(argv[2] , "-") == 0) {
+	else if (strcmp(argv[2] , "-") == 0){
 		printf("%s\n",argv[2]);
-		int result = sub (number1 , number2);
-		printf ("sub is = %d\n", result);
+		printf("Sub = %d \n",sub(number1,number2));
 	}
-	else if (strcmp(argv[2] , "*") == 0) {
-		int result = mult (number1 , number2);
-		printf ("product is = %d\n", result);
+	else if (strcmp(argv[2] , "*") == 0){
+		printf("product = %d \n",mult(number1,number2));
 	}
-	else if (strcmp(argv[2] , "/") == 0) {
-		int result = division (number1 , number2);
-		printf ("quotient  is = %d\n", result);
+	else if (strcmp(argv[2] , "/") == 0){
+		printf("quotient = %d \n",division(number1,number2));
 	}
 	else{
 		printf("enter right arithmetic operators\n");
---
+++ oprations.h
@@ -0,0 +1,15 @@
+int add(int number1, int number2){
+	return number1 + number2;
+}
+
+int sub(int number1, int number2){
+	return number1 - number2;
+}
+
+int mult(int number1, int number2){
+	return number1 * number2;
+}
+
+int division(int number1, int number2){
+	return number1 / number2;
+}
